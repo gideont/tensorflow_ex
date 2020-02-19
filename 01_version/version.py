83a7723d98b5d104a3ca3496cf660c6a5367658f
Gideon Tsang <gideont@gmail.com>
2020-02-19 22:22:54 +0800

    Updated the version print for Tensorflow 2

--- 01_version/version.py
+++ 01_version/version.py
@@ -3,5 +3,5 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
-print("TF version is: %s" % tf.VERSION)
+print("TF version is: %s" % tf.__version__)
 print("Keras version is: %s" % tf.keras.__version__)
