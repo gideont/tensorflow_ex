65e0ffd1dd1868e8a4ba3f5d80e1f0cb976a9db0
Gideon Tsang <gideontsang@computime.com>
2020-02-21 08:17:01 +0800

    Adding CUDA and GPU check in version.py

--- 01_version/version.py
+++ 01_version/version.py
@@ -1,7 +1,13 @@
-#!/usr/bin/python
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # or any {'0', '1', '2'}
 
 import tensorflow as tf
 from tensorflow.keras import layers
 
 print("TF version is: %s" % tf.__version__)
 print("Keras version is: %s" % tf.keras.__version__)
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+
+print("Build with CUDA: %s" % tf.test.is_built_with_cuda())
+
